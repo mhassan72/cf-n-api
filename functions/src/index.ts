879540a58eb7d3c9cfb86ef29f639b6df5d7e803
mohamedsugroo <mohamedsugroo@gmail.com>
2022-07-19 20:33:36 +0100

    . . .

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
 import * as functions from "firebase-functions";
 import App from "./app";
-export const apiV1 = functions.https.onRequest(App);
+export const Api_V1 = functions.https.onRequest(App);
 
